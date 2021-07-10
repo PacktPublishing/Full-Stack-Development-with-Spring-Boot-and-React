f278b312ac230ed655466da67ad5e03ce741d77e
juhahinkula <hinkula@gmail.com>
2021-07-10 15:38:42 +0300

    Chapter03 added

---
+++ Chapter03/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,54 @@
+package com.packt.cardatabase;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+
+@SpringBootApplication
+public class CardatabaseApplication implements CommandLineRunner {	
+	private static final Logger logger = 
+			LoggerFactory.getLogger(CardatabaseApplication.class);
+	  
+    @Autowired
+    private CarRepository repository;
+    
+
+    @Autowired
+    private OwnerRepository orepository;
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Add owner objects and save these to db 
+		Owner owner1 = new Owner("John" , "Johnson");
+		Owner owner2 = new  Owner("Mary" , "Robinson");
+		orepository.saveAll(Arrays.asList(owner1, owner2));
+
+		// Add car object and link to owners and save these to db
+		Car car1 = new Car("Ford", "Mustang", "Red", 
+				"ADF-1121", 2021, 59000, owner1);
+		Car car2 = new Car("Nissan", "Leaf", "White", 
+				"SSJ-3002", 2019, 29000, owner2);
+		Car car3 = new Car("Toyota", "Prius", "Silver", 
+				"KKO-0212", 2020, 39000, owner2);
+		repository.saveAll(Arrays.asList(car1, car2, car3));
+                  
+		for (Car car : repository.findAll()) {
+			logger.info(car.getBrand() + " " + car.getModel());
+		}
+	}
+}
