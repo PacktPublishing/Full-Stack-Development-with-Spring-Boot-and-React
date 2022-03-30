0191334fabeaeea6426c6a4971281d6d515c94fa
juhahinkula <hinkula@gmail.com>
2022-03-30 11:50:16 +0300

    React 18 upgrade

--- Chapter09/shoppinglist/src/index.js
+++ Chapter09/shoppinglist/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
